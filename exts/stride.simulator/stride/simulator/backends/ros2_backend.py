f4b4f7cfb9a58aaa67a3bf6774f6fa1adefcaf1a
jinwon kim <mqjinwon@gmail.com>
2024-02-19 00:26:39 +0900

    fix: pylint error

--- exts/stride.simulator/stride/simulator/backends/ros2_backend.py
+++ exts/stride.simulator/stride/simulator/backends/ros2_backend.py
@@ -1,25 +1,52 @@
+import carb
+from stride.simulator.backends.backend import Backend
 from omni.isaac.core.utils.extensions import disable_extension, enable_extension
 
 # # Perform some checks, because Isaac Sim some times does not play nice when using ROS/ROS2
 disable_extension("omni.isaac.ros_bridge")
 enable_extension("omni.isaac.ros2_bridge")
 
-import carb
-import omni.kit.app
-
 # Inform the user that now we are actually import the ROS2 dependencies
 # Note: we are performing the imports here to make sure that ROS2 extension was load correctly
-import rclpy
-from std_msgs.msg import Float64
-from sensor_msgs.msg import Imu, MagneticField, NavSatFix, NavSatStatus
-from geometry_msgs.msg import PoseStamped, TwistStamped, AccelStamped
-
-from stride.simulator.backends.backend import Backend
+import rclpy  # pylint: disable=wrong-import-position
+from std_msgs.msg import Float64  # pylint: disable=unused-import, wrong-import-position
+from sensor_msgs.msg import (  # pylint: disable=unused-import, wrong-import-position
+    Imu, MagneticField, NavSatFix, NavSatStatus,
+)
+from geometry_msgs.msg import PoseStamped, TwistStamped, AccelStamped  # pylint: disable=wrong-import-position
 
 
 class ROS2Backend(Backend):
+    """
+    A class representing the ROS2 backend for the simulation.
+
+    Args:
+        node_name (str): The name of the ROS2 node.
+
+    Attributes:
+        node: The ROS2 node object.
+        pose_pub: Publisher for the state of the vehicle in ENU.
+        twist_pub: Publisher for the state of the vehicle's twist in ENU.
+        twist_inertial_pub: Publisher for the state of the vehicle's inertial twist.
+        accel_pub: Publisher for the state of the vehicle's acceleration.
+        imu_pub: Publisher for the IMU sensor data.
+
+    Methods:
+        update(dt: float): Updates the state of the backend and the information being sent/received 
+                            from the communication interface.
+        update_imu_data(data): Updates the IMU sensor data.
+        update_sensor(sensor_type: str, data): Handles the receival of sensor data.
+        update_state(state): Handles the receival of the state of the vehicle.
+    """
 
     def __init__(self, node_name: str):
+        """
+        Initializes the ROS2Backend.
+
+        Args:
+            node_name (str): The name of the ROS2 node.
+        """
+        super().__init__()
 
         # Start the actual ROS2 setup here
         if not rclpy.ok():  # Check if the ROS2 context is already initialized
@@ -40,6 +67,9 @@ class ROS2Backend(Backend):
         Method that when implemented, should be used to update the state of the backend and the information being
         sent/received from the communication interface. This method will be called by the simulation on every physics
         step.
+
+        Args:
+            dt (float): The time step for the update.
         """
 
         # In this case, do nothing as we are sending messages as soon as new data arrives from the sensors and state
@@ -48,6 +78,12 @@ class ROS2Backend(Backend):
         rclpy.spin_once(self.node, timeout_sec=0)
 
     def update_imu_data(self, data):
+        """
+        Updates the IMU sensor data.
+
+        Args:
+            data: The IMU sensor data.
+        """
 
         msg = Imu()
 
@@ -70,7 +106,11 @@ class ROS2Backend(Backend):
 
     def update_sensor(self, sensor_type: str, data):
         """
-        Method that when implemented, should handle the receival of sensor data
+        Method that when implemented, should handle the receival of sensor data.
+
+        Args:
+            sensor_type (str): The type of the sensor.
+            data: The sensor data.
         """
 
         if sensor_type == "Imu":
@@ -81,7 +121,10 @@ class ROS2Backend(Backend):
 
     def update_state(self, state):
         """
-        Method that when implemented, should handle the receivel of the state of the vehicle using this callback.
+        Method that when implemented, should handle the receival of the state of the vehicle using this callback.
+
+        Args:
+            state: The state of the vehicle.
         """
 
         pose = PoseStamped()
@@ -90,7 +133,7 @@ class ROS2Backend(Backend):
         accel = AccelStamped()
 
         # Update the header
-        pose.header.stamp = self.node.get_clock().now().to_msg()  # TODO: fill time when the state was measured.
+        pose.header.stamp = (self.node.get_clock().now().to_msg())  # TODO: fill time when the state was measured.
         twist.header.stamp = pose.header.stamp
         twist_inertial.header.stamp = pose.header.stamp
         accel.header.stamp = pose.header.stamp
@@ -135,19 +178,19 @@ class ROS2Backend(Backend):
         self.twist_inertial_pub.publish(twist_inertial)
         self.accel_pub.publish(accel)
 
-    def check_ros_extension(self):
-        """
-        Method that checks which ROS extension is installed.
-        """
+    # def check_ros_extension(self):
+    #     """
+    #     Method that checks which ROS extension is installed.
+    #     """
 
-        # Get the handle for the extension manager
-        extension_manager = omni.kit.app.get_app().get_extension_manager()
+    #     # Get the handle for the extension manager
+    #     extension_manager = omni.kit.app.get_app().get_extension_manager()
 
-        version = ""
+    #     version = ""
 
-        if self._ext_manager.is_extension_enabled("omni.isaac.ros_bridge"):
-            version = "ros"
-        elif self._ext_manager.is_extension_enabled("omni.isaac.ros2_bridge"):
-            version = "ros2"
-        else:
-            carb.log_warn("Neither extension 'omni.isaac.ros_bridge' nor 'omni.isaac.ros2_bridge' is enabled")
+    #     if self._ext_manager.is_extension_enabled("omni.isaac.ros_bridge"):
+    #         version = "ros"
+    #     elif self._ext_manager.is_extension_enabled("omni.isaac.ros2_bridge"):
+    #         version = "ros2"
+    #     else:
+    #         carb.log_warn("Neither extension 'omni.isaac.ros_bridge' nor 'omni.isaac.ros2_bridge' is enabled")
